469c5dde078d5cdd0073094fa7205d61ab4eb0a0
Adwithya <avmagow@gmail.com>
2019-08-02 12:00:43 +0700

    Added bootstrap 4 and made login and register pages

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,23 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get("/", function(req, res, next) {
+  res.send("Working on Page");
+});
+
+//Login Page
+router.get("/login", function(req, res, next) {
+  res.render("login", {
+    title: "Login"
+  });
+});
+
+//Registration Page
+router.get("/register", function(req, res, next) {
+  res.render("register", {
+    title: "Register"
+  });
 });
 
 module.exports = router;
